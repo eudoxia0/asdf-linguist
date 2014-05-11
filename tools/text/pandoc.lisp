868de9a3ef418138440447aa59314f6b14299198
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-11 11:09:39 -0300

    Change pandoc code

--- tools/text/pandoc.lisp
+++ tools/text/pandoc.lisp
@@ -2,8 +2,10 @@
 
 (defclass pandoc (source-file)
   ((type :initarg :type :reader input-type)
+   (input-format :initarg :input-type :reader input-format)
    (output :initarg :output :reader output)
    (output-type :initarg :output-type :reader output-type)
+   (output-extension :initarg :output-extension :reader output-extension)
    (options :initarg :options :reader options :initform "")))
 
 (defmethod perform ((o load-op) (component pandoc)) t)
@@ -11,14 +13,14 @@
 (defmethod output-files ((operation compile-op) (component pandoc))
   (values
    (list
-    (out component (output-type component)))))
+    (out component (output-extension component)))))
 
 (defmethod perform ((o compile-op) (component pandoc))
-  (run "pandoc -t ~A -o ~A ~A ~A"
+  (run "pandoc -t ~A -f ~A -o ~A ~A ~A"
        (output-type component)
-       (namestring (out component (output-type component)))
+       (input-format component)
+       (namestring (out component (output-extension component)))
        (namestring (component-pathname component))
        (options component)))
-
 
 (import 'pandoc :asdf)
