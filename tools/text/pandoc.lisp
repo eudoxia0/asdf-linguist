d91ef6ceb8bfc61c0bef22679140ba7f508b312e
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-17 11:14:15 -0300

    Changes to pandoc extension

--- tools/text/pandoc.lisp
+++ tools/text/pandoc.lisp
@@ -4,20 +4,21 @@
   ((type :initarg :type :reader input-type)
    (input-format :initarg :input-type :reader input-format)
    (output :initarg :output :reader output)
-   (output-type :initarg :output-type :reader output-type)
+   (output-type :initarg :output-type :reader output-type :initform nil)
    (output-extension :initarg :output-extension :reader output-extension)
    (options :initarg :options :reader options :initform "")))
 
-(defmethod perform ((o load-op) (component pandoc)) t)
+(defmethod perform ((o load-op) (component pandoc)) nil)
 
 (defmethod output-files ((operation compile-op) (component pandoc))
-  (values
-   (list
-    (out component (output-extension component)))))
+  (list
+   (out component (output-extension component))))
 
 (defmethod perform ((o compile-op) (component pandoc))
-  (run "pandoc -t ~A -f ~A -o ~A ~A ~A"
-       (output-type component)
+  (run "pandoc ~A -f ~A -o ~A ~A ~A"
+       (if (output-type component)
+           (concatenate 'string "-t " (output-type component))
+           "")
        (input-format component)
        (namestring (out component (output-extension component)))
        (namestring (component-pathname component))
